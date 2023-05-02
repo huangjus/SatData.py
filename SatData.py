9127dc72f8907d296b58850b7ccf1925e4c28c08
huangjus <122409377+huangjus@users.noreply.github.com>
2023-05-02 15:04:55 -0700

    Update SatData.py

--- SatData.py
+++ SatData.py
@@ -30,18 +30,17 @@ class SatData:
         The rows in the CSV file are sorted in ascending order by DBN.
         """
 
-        filtered_data = sorted([row for row in self.__data if row["DBN"] in dbns], key=lambda x: x["DBN"])
+        filtered_data = sorted([row for row in self.__data if row.get("DBN") in dbns], key=lambda x: x.get("DBN"))
 
         with open("output.csv", "w") as csvfile:
             csvfile.write(
                 "DBN,School Name,Number of Test Takers,Critical Reading Mean,Mathematics Mean,Writing Mean\n")
 
             for row in filtered_data:
-                school_name = row["School Name"]
+                school_name = row.get("School Name")
                 if ',' in school_name:
                     school_name = f'"{school_name}"'
 
                 csvfile.write(
-                    f'{row["DBN"]},{school_name},{row["Number of Test Takers"]},'
-                    f'{row["Critical Reading Mean"]},{row["Mathematics Mean"]},'
-                    f'{row["Writing Mean"]}\n')
+                    f'{row.get("DBN")},{school_name},{row.get("Number of Test Takers")},'
+                    f'{row.get("Critical Reading Mean")},{row.get("Mathematics Mean")},{row.get("Writing Mean")}\n')
